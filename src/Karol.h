2a5c99e7390b0c3c7f3ca77a760bfb825840b98e
codeleaded <codeleaded@gmail.com>
2025-11-03 16:24:58 +0100

    update

---
+++ src/Karol.h
@@ -0,0 +1,103 @@
+#ifndef KAROL_H
+#define KAROL_H
+
+#include "/home/codeleaded/System/Static/Library/Lib3D_Cube.h"
+#include "/home/codeleaded/System/Static/Library/Lib3D_Mathlib.h"
+#include "/home/codeleaded/System/Static/Library/Lib3D_Mesh.h"
+
+typedef struct BodyPart {
+    vec3d p;
+    vec3d a;
+    Vector mesh;
+} BodyPart;
+
+BodyPart BodyPart_New(vec3d a,vec3d p,short sides,vec3d start,vec3d l,unsigned int col){
+    BodyPart bp;
+    bp.p = p;
+    bp.a = a;
+    bp.mesh = Vector_New(sizeof(triangle));
+	Lib3D_Sides(&bp.mesh,sides,start,l,col,col);
+    return bp;
+}
+void BodyPart_Free(BodyPart* bp){
+    Vector_Free(&bp->mesh);
+}
+
+
+
+typedef SharedPointer KarolPtr;
+
+typedef struct Karol {
+    vec3d p;
+    vec3d d;
+    vec3d a;
+    Vector bodyparts;
+} Karol;
+
+// Head Body Arm(1,2) Leg(1,2) Feet(1,2)
+void Karol_StdFigur(Karol* k,float xl,float yl,float zl,unsigned int c1,unsigned int c2,unsigned int c3,unsigned int c4,unsigned int c5,unsigned int c6,unsigned int c7,unsigned int c8) {
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ 0.0f,-yl/8.0f,0.0f                },0,                                              (vec3d){ -xl/12.0f*4.0f,0.0f,-zl/12.0f*4.0f           },(vec3d){ xl/12.0f*8.0f,yl/4.0f,zl/12.0f*8.0f      },c1) });
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ 0.0f,-yl/8.0f*4,0.0f              },0,                                              (vec3d){ -xl/12.0f*4.0f,0.0f,-zl/12.0f*2.0f           },(vec3d){ xl/12.0f*8.0f,yl/8.0f*3.0f,zl/12.0f*4.0f },c2) });
+	
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ xl/2.0f,-yl/16.0f*3.0f,0.0f       },0,                                              (vec3d){ -xl/12.0f*2.0f,-yl/16.0f*5.0f,-zl/12.0f*2.0f },(vec3d){ xl/12.0f*4.0f,yl/8.0f*3.0f,zl/12.0f*4.0f },c3) });
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ -xl/2.0f,-yl/16.0f*3.0f,0.0f      },0,                                              (vec3d){ -xl/12.0f*2.0f,-yl/16.0f*5.0f,-zl/12.0f*2.0f },(vec3d){ xl/12.0f*4.0f,yl/8.0f*3.0f,zl/12.0f*4.0f },c4) });
+	
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ xl/12.0f*2.0f,-yl/8.0f*4.0f,0.0f  },(LIB3D_CUBE_S0_BOTTOM | LIB3D_CUBE_S1_BOTTOM),  (vec3d){ -xl/12.0f*2.0f,-yl/8.0f*2.0f,-zl/12.0f*2.0f  },(vec3d){ xl/12.0f*4.0f,yl/8.0f*2.0f,zl/12.0f*4.0f },c5) });
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ -xl/12.0f*2.0f,-yl/8.0f*4.0f,0.0f },(LIB3D_CUBE_S0_BOTTOM | LIB3D_CUBE_S1_BOTTOM),  (vec3d){ -xl/12.0f*2.0f,-yl/8.0f*2.0f,-zl/12.0f*2.0f  },(vec3d){ xl/12.0f*4.0f,yl/8.0f*2.0f,zl/12.0f*4.0f },c6) });
+
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ xl/12.0f*2.0f,-yl/8.0f*4.0f,0.0f  },(LIB3D_CUBE_S0_TOP | LIB3D_CUBE_S1_TOP),        (vec3d){ -xl/12.0f*2.0f,-yl/8.0f*3.0f,-zl/12.0f*2.0f  },(vec3d){ xl/12.0f*4.0f,yl/8.0f,zl/12.0f*4.0f      },c7) });
+	Vector_Push(&k->bodyparts,(BodyPart[]){ BodyPart_New((vec3d){0.0f,0.0f,0.0f}, (vec3d){ -xl/12.0f*2.0f,-yl/8.0f*4.0f,0.0f },(LIB3D_CUBE_S0_TOP | LIB3D_CUBE_S1_TOP),        (vec3d){ -xl/12.0f*2.0f,-yl/8.0f*3.0f,-zl/12.0f*2.0f  },(vec3d){ xl/12.0f*4.0f,yl/8.0f,zl/12.0f*4.0f      },c8) });
+}
+Karol Karol_New(vec3d p,vec3d d){
+    Karol k;
+    k.p = p;
+    k.d = d;
+    k.a = (vec3d){ 0.0f,0.0f,0.0f,1.0f };
+    k.bodyparts = Vector_New(sizeof(BodyPart));
+    Karol_StdFigur(&k,d.x,d.y,d.z,0xFFAAAA00,0xFF000088,0xFF003399,0xFF003399,0xFF00FF00,0xFF00FF00,0xFF777777,0xFF777777);
+    return k;
+}
+void Karol_Add(Karol* k,Vector* tris){
+    mat4x4 matY = Matrix_MakeRotationY(k->a.y);
+    mat4x4 matX = Matrix_MakeRotationX(k->a.x);
+    
+	for(int i = 0;i<k->bodyparts.size;i++){
+        BodyPart* bp = (BodyPart*)Vector_Get(&k->bodyparts,i);
+        
+        for(int j = 0;j<bp->mesh.size;j++) {
+            triangle* tri = (triangle*)Vector_Get(&bp->mesh,j);
+
+			mat4x4 matTurnX = Matrix_MakeRotationX(bp->a.x);
+			mat4x4 matTurnY = Matrix_MakeRotationY(bp->a.y);
+			mat4x4 matTurnZ = Matrix_MakeRotationZ(bp->a.z);
+			
+	        triangle triRotated;
+            triRotated.p[0] = vec3d_Add(bp->p,Matrix_MultiplyVector(matTurnZ,Matrix_MultiplyVector(matTurnY,Matrix_MultiplyVector(matTurnX,tri->p[0]))));
+            triRotated.p[1] = vec3d_Add(bp->p,Matrix_MultiplyVector(matTurnZ,Matrix_MultiplyVector(matTurnY,Matrix_MultiplyVector(matTurnX,tri->p[1]))));
+            triRotated.p[2] = vec3d_Add(bp->p,Matrix_MultiplyVector(matTurnZ,Matrix_MultiplyVector(matTurnY,Matrix_MultiplyVector(matTurnX,tri->p[2]))));
+
+            triRotated.p[0] = Matrix_MultiplyVector(matY,triRotated.p[0]);
+            triRotated.p[1] = Matrix_MultiplyVector(matY,triRotated.p[1]);
+            triRotated.p[2] = Matrix_MultiplyVector(matY,triRotated.p[2]);
+
+            triRotated.p[0] = vec3d_Add(k->p,triRotated.p[0]);
+            triRotated.p[1] = vec3d_Add(k->p,triRotated.p[1]);
+            triRotated.p[2] = vec3d_Add(k->p,triRotated.p[2]);
+            triRotated.c = tri->c;
+
+            triangle_CalcNorm(&triRotated);
+            //triangle_ShadeNorm(&triRotated,(vec3d){ 0.6f,0.7f,0.8f });
+	    	
+            Vector_Push(tris,&triRotated);
+		}
+	}
+}
+void Karol_Free(Karol* k){
+    for(int i = 0;i<k->bodyparts.size;i++){
+        BodyPart* bp = (BodyPart*)Vector_Get(&k->bodyparts,i);
+        BodyPart_Free(bp);
+    }
+    Vector_Free(&k->bodyparts);
+}
+
+#endif
--- src/Main.c
+++ src/Main.c
@@ -1,15 +1,171 @@
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
 #include "/home/codeleaded/System/Static/Library/LuaLike.h"
+#include "/home/codeleaded/System/Static/Library/TransformedView.h"
+#include "/home/codeleaded/System/Static/Library/Lib3D_Cube.h"
+#include "/home/codeleaded/System/Static/Library/Lib3D_Mathlib.h"
+#include "/home/codeleaded/System/Static/Library/Lib3D_Mesh.h"
 
-int main(int argc,char** argv){
+#include "./Karol.h"
+
+#define FIELD_SIZEX  10
+#define FIELD_SIZEY  10
+#define FIELD_SIZEZ  10
+
+
+TransformedView tv;
+LuaLike ll;
+
+Camera cam;
+World3D world;
+int Mode = 0;
+int Menu = 0;
+float Speed = 4.0f;
+
+void Menu_Set(int m){
+	if(Menu==0 && m==1){
+		AlxWindow_Mouse_SetInvisible(&window);
+		SetMouse((Vec2){ GetWidth() / 2,GetHeight() / 2 });
+	}
+	if(Menu==1 && m==0){
+		AlxWindow_Mouse_SetVisible(&window);
+	}
+	
+	Menu = m;
+}
+
+void Setup(AlxWindow* w){
+	ll = LuaLike_New("./code/Main.ll","./bin");
+	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
+
+    cam = Camera_Make(
+		(vec3d){ FIELD_SIZEX,8.0f,FIELD_SIZEZ,1.0f },
+		(vec3d){ 0.0f,0.0f,0.0f,1.0f },
+		(vec3d){ 3.14 * 0.25f,3.14 * 0.75f,0.0f,1.0f },
+		90.0f
+	);
+
+	world = World3D_Make(
+		Matrix_MakeWorld((vec3d){ 0.0f,0.0f,0.0f,1.0f },(vec3d){ 0.0f,0.0f,0.0f,1.0f }),
+		Matrix_MakePerspektive(cam.p,cam.up,cam.a),
+		Matrix_MakeProjection(cam.fov,(float)GetHeight() / (float)GetWidth(),0.1f,1000.0f)
+	);
+	world.normal = WORLD3D_NORMAL_CAP;
+
+    Menu_Set(1);
+}
+
+void Update(AlxWindow* w){
+    if(Stroke(ALX_KEY_SPACE).PRESSED){
+        const float x = 0.0f;
+        const float y = 0.0f;
+        const float z = 0.0f;
+
+        Karol* pw = (Karol*)malloc(sizeof(Karol));
+        *pw = Karol_New((vec3d){ x + 0.75f * 0.75f,y + 1.75f,z + 0.75f * 0.75f },(vec3d){ 0.75f,2.0f,0.75f });
+        KarolPtr ptr = SharedPointer_Make(pw,(void*)Karol_Free);
+
+        CStr name = Enviroment_Variablename_Next(&ll.ev,".KAROL",6);
+        Scope_BuildInitVariableRange(&ll.ev.sc,name,"karol",0,&ptr);
+        CStr_Free(&name);
+    }
+    if(Stroke(ALX_KEY_ENTER).PRESSED){
+        Enviroment_ClearScript(&ll.ev);
+        Enviroment_Script(&ll.ev,"./code/Main.ll");
+
+        Variable ret = LuaLike_Function(&ll,"main",(Variable[]){ 
+            VARIABLE_END
+        });
+
+        // Variable_Print(&ret);
+        // printf("\n");
+
+        Variable_Free(&ret);
+    }
+
+    if(Menu==1){
+		Camera_Focus(&cam,GetMouseBefore(),GetMouse(),GetScreenRect().d);
+		Camera_Update(&cam);
+		SetMouse((Vec2){ GetWidth() / 2,GetHeight() / 2 });
+	}
+	
+	if(Stroke(ALX_KEY_ESC).PRESSED)
+		Menu_Set(!Menu);
+
+	if(Stroke(ALX_KEY_Z).PRESSED)
+		Mode = Mode < 2 ? Mode + 1 : 0;
+
+	if(Stroke(ALX_KEY_W).DOWN)
+		cam.p = vec3d_Add(cam.p,vec3d_Mul(cam.ld,Speed * w->ElapsedTime));
+	if(Stroke(ALX_KEY_S).DOWN)
+		cam.p = vec3d_Sub(cam.p,vec3d_Mul(cam.ld,Speed * w->ElapsedTime));
+	if(Stroke(ALX_KEY_A).DOWN)
+		cam.p = vec3d_Add(cam.p,vec3d_Mul(cam.sd,Speed * w->ElapsedTime));
+	if(Stroke(ALX_KEY_D).DOWN)
+		cam.p = vec3d_Sub(cam.p,vec3d_Mul(cam.sd,Speed * w->ElapsedTime));
+	if(Stroke(ALX_KEY_R).DOWN)
+		cam.p.y += Speed * w->ElapsedTime;
+	if(Stroke(ALX_KEY_F).DOWN)
+		cam.p.y -= Speed * w->ElapsedTime;
+
+
+    tv.ZoomSpeed = 1.0f * w->ElapsedTime;
+	TransformedView_HandlePanZoom(&tv,window.Strokes,GetMouse());
+
+    //World3D_Set_Model(&world,Matrix_MakeWorld((vec3d){ 0.0f,0.0f,0.0f,1.0f },(vec3d){ 0.0f,0.0f,0.0f,1.0f }));
+	World3D_Set_View(&world,Matrix_MakePerspektive(cam.p,cam.up,cam.a));
+	World3D_Set_Proj(&world,Matrix_MakeProjection(cam.fov,(float)GetHeight() / (float)GetWidth(),0.1f,1000.0f));
+
+	Clear(LIGHT_BLUE);
+
+
+    Vector_Clear(&world.trisIn);
+	for(int i = 0;i<FIELD_SIZEZ;i++){
+		for(int j = 0;j<FIELD_SIZEX;j++){
+		    Lib3D_Sides(&world.trisIn,~((short)LIB3D_CUBE_S0_TOP | (short)LIB3D_CUBE_S1_TOP),(vec3d){ i + 0.05f,-1.0f,j + 0.05f },(vec3d){ 0.9f,1.0f,0.9f },DARK_BLUE,DARK_BLUE);
+	    }
+	}
     
-    LuaLike ll = LuaLike_New("./code/Main.ll","./bin");
-    Variable ret = LuaLike_Function(&ll,"main",(Variable[]){ 
-        VARIABLE_END
-    });
-    LuaLike_PrintVariable(&ll,&ret);
-    Variable_Free(&ret);
-    
+    for(int i = 0;i<ll.ev.sc.vars.size;i++){
+        Variable* var = (Variable*)PVector_Get(&ll.ev.sc.vars,i);
+        
+        if(CStr_Cmp(var->typename,"karol")){
+            KarolPtr* wptr = (KarolPtr*)Variable_Data(var);
+            Karol* k = (Karol*)wptr->Memory;
+
+            Karol_Add(k,&world.trisIn);
+        }
+    }
+
+	World3D_update(&world,cam.p,(Vec2){ GetWidth(),GetHeight() });
+
+	for(int i = 0;i<world.trisOut.size;i++){
+		triangle* t = (triangle*)Vector_Get(&world.trisOut,i);
+
+		if(Mode==0)
+			RenderTriangle(((Vec2){ t->p[0].x, t->p[0].y }),((Vec2){ t->p[1].x, t->p[1].y }),((Vec2){ t->p[2].x, t->p[2].y }),t->c);
+		if(Mode==1)
+			RenderTriangleWire(((Vec2){ t->p[0].x, t->p[0].y }),((Vec2){ t->p[1].x, t->p[1].y }),((Vec2){ t->p[2].x, t->p[2].y }),t->c,1.0f);
+		if(Mode==2){
+			RenderTriangle(((Vec2){ t->p[0].x, t->p[0].y }),((Vec2){ t->p[1].x, t->p[1].y }),((Vec2){ t->p[2].x, t->p[2].y }),t->c);
+			RenderTriangleWire(((Vec2){ t->p[0].x, t->p[0].y }),((Vec2){ t->p[1].x, t->p[1].y }),((Vec2){ t->p[2].x, t->p[2].y }),WHITE,1.0f);
+		}
+	}
+
+	String str = String_Format("PX:%f, PY:%f, PZ:%f",cam.p.x,cam.p.y,cam.p.z);
+	RenderCStrSize(str.Memory,str.size,0.0f,0.0f,WHITE);
+	String_Free(&str);
+
+	str = String_Format("%d(%d): AX:%f, AY:%f",world.trisIn.size,Mode,cam.a.x,cam.a.y);
+	RenderCStrSize(str.Memory,str.size,0.0f,GetAlxFont()->CharSizeY,WHITE);
+	String_Free(&str);
+}
+void Delete(AlxWindow* w){
     LuaLike_Free(&ll);
-    
+    World3D_Free(&world);
+}
+
+int main(){
+    if(Create("LL - Karol",1920,1080,1,1,Setup,Update,Delete))
+        Start();
     return 0;
 }
